ea8faaf57fad70cf38edb6874a68d1d0be3b3060
ンテ Dehanjing <dehanmek@gmail.com>
2021-05-11 13:27:21 +0800

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,32 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/docs', (req, res) => {
     res.sendFile(__path + '/views/dashboard.html')
 })
 
-router.get('/about', (req, res) => {
-    res.sendFile(__path + '/views/about.html')
+router.get('/docs/account', (req, res) => {
+    res.sendFile(__path + '/views/accounts.html')
+})
+
+router.get('/docs/changelog', (req, res) => {
+    res.sendFile(__path + 'views/typography.html')
+})
+
+router.get('/docs/user', (req, res) => {
+    res.sendFile(__path + '/views/user.html')
+})
+
+router.get('/tutor', (req, res) => {
+    res.sendFile(__path + '/views/tutor.html')
+})
+
+router.get('/login', (req, res) => {
+    res.sendFile(__path + '/views/login.html')
+})
+
+router.get('/docs/notification', (req, res) => {
+    res.sendFile(__path + '/views/notifications.html')
 })
 
 router.get('/config', (req, res) => {
@@ -16,10 +36,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'Dehan • BOT',
+            namaowner: 'Dehanjing',
+            instagram: 'dehan_j1ng',
+            youtube : 'ンテ Dehanjing'
         }
     }
     res.json(config)
